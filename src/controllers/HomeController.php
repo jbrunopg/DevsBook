d7889fe6ceb2045f01e14799247c887497440f7b
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2023-05-01 00:07:07 -0300

    Commit p/ criação tela login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,12 +2,20 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\HandlerLogin;
+
 
 class HomeController extends Controller {
 
     private $loggedUser;
 
     public function __construct() {
+        $this->loggedUser = HandlerLogin::checkLogin();
+        if(HandlerLogin::checkLogin( == false)){
+            $this->redirect('/login');
+        }
+
+
         $this->redirect('/login');
     }
 
---
+++ src/handlers/HandlerLogin.php
@@ -0,0 +1,7 @@
+<?php
+namespace src\handlers;
+
+
+class HandlerLogin {
+
+}
